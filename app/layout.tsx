af3e0fa94e9c5fd48137088ba73c23d376c2521b
Lucas Aschenbach <lucas.aschenbach@gmail.com>
2025-05-31 01:15:38 +0200

    Update website meta-data

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Github Pages Explorer',
+  description: 'Browse all your hosted GitHub Pages repositories',
+  generator: 'github-pages-explorer',
 }
 
 export default function RootLayout({
